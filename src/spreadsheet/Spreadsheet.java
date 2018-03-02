0f1d60c18286eb93d6e2e6ffd154a949e5466a2a
nc1517 <nc1517@ic.ac.uk>
2018-03-02 12:22:30 +0000

    Not much has changed

--- src/spreadsheet/Spreadsheet.java
+++ src/spreadsheet/Spreadsheet.java
@@ -2,11 +2,14 @@ package spreadsheet;
 
 import common.api.CellLocation;
 import common.api.Tabular;
+import common.api.value.InvalidValue;
+import common.api.value.LoopValue;
 import common.api.value.StringValue;
 import common.api.value.Value;
 import common.api.value.ValueEvaluator;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -38,7 +41,7 @@ public class Spreadsheet implements Tabular {
 
   @Override
   public Value getValue(CellLocation location) {
-    if(cells.containsKey(location)) {
+    if (cells.containsKey(location)) {
       return cells.get(location).getValue();
     } else {
       return null;
@@ -46,11 +49,83 @@ public class Spreadsheet implements Tabular {
   }
 
   @Override
-  public void recompute(){
-  while(!recompCells.isEmpty()){  //no for loop cause length will change due to removal of cells
-    Cell cell = recompCells.get(0);
-    cell.setValue(new StringValue(cell.getExpression()));
-    recompCells.remove(cell);
+  public void recompute() {
+    while (!recompCells.isEmpty()) {  //no for loop cause length will change due to removal of cells
+      Cell cell = recompCells.get(0);
+      cell.setValue(new StringValue(cell.getExpression()));
+      recompCells.remove(cell);
+      //include call to recomputeCell
+    }
+  }
+
+  private void recomputeCell(Cell c) {
+    //call to checkLoops to check if there are any loops inside
+  }
+
+  private void checkLoops(Cell c, LinkedHashSet<Cell> cellsSeen) {
+    if (cellsSeen.contains(c)) {
+      //call to markAsValidatedLoop
+    }
+  }
+
+  //markAsValidatedLoop has to run recursively on the cells the startCell depends on
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* private void markAsValidatedLoop(Cell startCell, LinkedHashSet<Cell> cells) { //noch verbessern
+   Set<Cell> beforeStart = new HashSet<>();
+   Set<Cell> afterStart = new HashSet<>();
+   for (Cell c : cells) {
+     if (c == startCell) {
+          break;
+     } else {
+       beforeStart.add(c);
+       c.setValue(new InvalidValue(c.getExpression()));
+       cells.remove(c);
+     }
    }
+   for (Cell c : cells) {
+     // c.setValue(new LoopValue()); //restlichen Elemente in cells sind alle ab startcell
+     afterStart.add(c);
+   }
+   cells.clear();
+   for(Cell c : beforeStart){
+     cells.add(c);
+   }
+   for(Cell c : afterStart){
+     cells.add(c);
+   }
+
  }
+*/
 }
